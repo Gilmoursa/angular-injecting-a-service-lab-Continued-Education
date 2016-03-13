6d9b2c459b12287dc50f281bce364e029244b396
Gilmoursa <gilmoursa@gmail.com>
2016-03-12 19:23:44 -0500

    all done

--- js/app/controllers/ContactController.js
+++ js/app/controllers/ContactController.js
@@ -1,5 +1,8 @@
-function ContactController($scope) {
-	$scope.name = 'Bill Gates';
+function ContactController($scope, $timeout) {
+	$scope.name = 'Austin';
+    $timeout(function(){
+        $scope.name = 'Greg';
+    },5000);
 }
 
 angular
